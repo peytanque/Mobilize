9d0fc2409cd4debd72b73fae49afff36d77739d2
Romain Durand ~ Peytanque <rdurand@keley.com>
2024-07-16 11:19:38 +0200

    init i18n

--- src/renderer/components/LanguageSelector.tsx
+++ src/renderer/components/LanguageSelector.tsx
@@ -1,27 +1,37 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { SlashSVG } from '../svg';
+import { useTranslation } from 'react-i18next';
 
 type Language = {
   code: string;
   name: string;
+  label?: string;
 };
 
 const languageList: Language[] = [
-  { code: 'en', name: 'english' },
-  { code: 'fr', name: 'français' },
+  { name: 'en', code: 'en-US', label: 'english' },
+  { name: 'fr', code: 'fr-FR', label: 'français' },
 ];
 
-const toggleLanguage = (language: Language) => {
-  console.log('selectedL', language);
-};
-
 export const LanguageSelector: FC = () => {
+  const { i18n } = useTranslation();
+  const [currentLanguage, setCurrentLanguage] = useState<string>(i18n.language ?? "")
+
+  const setLanguage = (language: Language) => {
+    i18n.changeLanguage(language.code)
+    setCurrentLanguage(language.code)
+  };
+
   return (
-    <div className="flex flex-row">
+    <div className="flex flex-row gap-5">
       {languageList.map((language, index) => (
         <>
-          <div key={language.name} onClick={() => toggleLanguage(language)} className='text-5xl	'>
-            {language.code.toUpperCase()}
+          <div
+            key={language.code}
+            onClick={() => setLanguage(language)}
+            className={`text-5xl ${currentLanguage === language.code && 'text-vermilion'}` }
+          >
+            {language.name.toUpperCase()}
           </div>
           {index + 1 < languageList.length && <SlashSVG />}
         </>
